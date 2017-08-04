0d69c40e0f71d365ae2a50e218db863d26311daf
Tony Laidig <laidig@gmail.com>
2017-08-04 16:05:22 -0400

    if all children station accessible, parent station is

--- mzgtfs-tools/add_accessible.py
+++ mzgtfs-tools/add_accessible.py
@@ -8,6 +8,7 @@ Also adds wheelchair_accessible=1 for all trips
 usage: python add_accessibility.py {gtfs_feed} {accessible_stops.json}
 """
 
+from collections import defaultdict
 import json
 import shutil
 import mzgtfs.feed
@@ -23,22 +24,53 @@ def main(gtfs_file, input_json_file):
         accessible_stop_count = len(accessible_stops)
 
     added_accessible_stop_count = 0
+    added_accessible_parent_count = 0 
+
+    parent_to_child_stations = defaultdict(set)
 
     for stop in feed_stops:
         if stop.id() in accessible_stops:
             stop.set('wheelchair_boarding', '1')
             added_accessible_stop_count += 1
-    
+        
+        parent = stop.get('parent_station')
+        if (parent and parent.strip()):
+            parent_to_child_stations[parent].add(stop.id())
+
+    # if the stop is a parent station and all substations are accessible, parent station is accessible, too
+    for parent, children in parent_to_child_stations.iteritems():
+        child_count = len(children)
+        accessible_count = 0
+        for c in children:
+            stop = gtfs_feed.stop(c)
+            if stop.get('wheelchair_boarding') == '1':
+                accessible_count += 1
+
+        if child_count is accessible_count:
+            gtfs_feed.stop(parent).set('wheelchair_boarding', '1')
+
     make_accessible_trips(gtfs_feed)
 
     files = ['stops.txt', 'trips.txt']
 
+    # nicely ordered output columns
     cols = ['stop_id','stop_lat','stop_lon','stop_name','wheelchair_boarding']
+    
+    # if there are any additional columns in the source data, add them here
+    all_columns = {column for s in feed_stops for column in s.keys()}
+    for c in all_columns:
+        if c not in cols:
+            cols.append(c)
+
+
     gtfs_feed.write('stops.txt', gtfs_feed.stops(), columns=cols)
 
-    print "added accessibility to {} stops from list of {}".format(added_accessible_stop_count, accessible_stop_count)
+    print "added accessibility to {} stops from an input list of {}".format(added_accessible_stop_count, accessible_stop_count)
+    if added_accessible_parent_count > 0:
+        print "and {} parent stations made accessible".format(added_accessible_parent_count)
 
     cols = ['route_id','trip_id','service_id','direction_id','trip_headsign','shape_id','wheelchair_accessible']
+
     gtfs_feed.write('trips.txt', gtfs_feed.trips(), columns=cols)
 
     gtfs_feed.make_zip('output.zip', files=files, clone=gtfs_file)
